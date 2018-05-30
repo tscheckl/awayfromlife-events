23de5fa6a32ab56080069bc359ab56c4158aa868
Philipp Jäckel <philipp.jaeckel96@googlemail.com>
2018-05-30 21:49:39 +0200

    finished advanced-search functions

--- src/components/ContentLists/EventsSearch.vue
+++ src/components/ContentLists/EventsSearch.vue
@@ -20,8 +20,8 @@
 				</div>
 
 				<div class="advanced-search">
-					<h3>Advanced Search <md-icon>keyboard_arrow_right</md-icon></h3>
-					<div class="search-options">
+					<h3 v-on:click="toggleAdvancedSearch(!showAdvancedSearch)">Advanced Search <md-icon>{{showAdvancedSearch ?'keyboard_arrow_down' :'keyboard_arrow_right'}}</md-icon></h3>
+					<div class="search-options hide-options">
 						<h4>Include Categories: </h4>
 						<div class="include-cate">
 							<md-checkbox v-model="searchOptions.includeCategories.events">Events</md-checkbox>
@@ -29,26 +29,33 @@
 							<md-checkbox v-model="searchOptions.includeCategories.bands">Bands</md-checkbox>
 						</div>
 						<h4>Only show results in one City or Country: </h4>
-						<md-button-toggle md-single class="md-accent">
-							<md-button class="md-button md-toggle" v-on:click="changeLocationOption('city')">
-								City
-							</md-button>
+						<div class="location-select">
+							<md-button-toggle md-single class="md-accent">
+								<md-button :class="'md-button ' + (locationOption=='city' ?'md-toggle' :'')" v-on:click="changeLocationOption('city')">
+									City
+								</md-button>
 
-							<md-button class="md-button" v-on:click="changeLocationOption('country')">
-								Country
-							</md-button>
-						</md-button-toggle>
+								<md-button :class="'md-button ' + (locationOption=='country' ?'md-toggle' :'')" v-on:click="changeLocationOption('country')">
+									Country
+								</md-button>
+							</md-button-toggle>
 
-						<md-input-container v-show="locationOption == 'city'">
-							<input  class="city_input" type="search" ref="city_input" v-model="searchOptions.city" placeholder="Select city" />
-						</md-input-container>
+							<md-input-container v-show="locationOption == 'city'">
+								<input  class="city-input" type="search" ref="city_input" v-model="searchOptions.city" placeholder="Select city" />
+							</md-input-container>
 
-						<md-input-container v-show="locationOption == 'country'">
-							<input class="country_input" type="search" ref="country_input" v-model="searchOptions.country" placeholder="Select country" />
-						</md-input-container>
+							<md-input-container v-show="locationOption == 'country'">
+								<input class="country-input" type="search" ref="country_input" v-model="searchOptions.country" placeholder="Select country" />
+							</md-input-container>
+						</div>
 
 						<h4>Only Show results with Genre: </h4>
-
+						<md-input-container class="genre-select">
+							<v-select :options="availableGenres"
+										v-model="searchOptions.genre"
+										placeholder="Select genre">
+							</v-select>
+						</md-input-container>
 					</div>
 				</div>
 			</div>
@@ -151,16 +158,19 @@ export default {
 			searched: false,
 			searchOptions: {
 				includeCategories: {
-					events: false,
-					locations: false,
-					bands: false
+					events: true,
+					locations: true,
+					bands: true
 				},
 				city: '',
-				country: ''
+				country: '',
+				genre: ''
 			},
 			locationOption: 'city',
 			placesAutocomplete1: {},
-			placesAutocomplete2: {}
+			placesAutocomplete2: {},
+			availableGenres: [],
+			showAdvancedSearch: false
 		}
 	},
 	methods: {
@@ -174,19 +184,24 @@ export default {
 		search() {
 			this.searched = false;
 			this.emptyResults();
+			this.toggleAdvancedSearch(false);
 
 			document.getElementsByClassName('page-content')[0].classList.remove('slide-up');
 			this.loading = true;
-
-			this.$http.get(backendUrl + '/api/search/' + this.query)
+			
+			this.$http.get(backendUrl + '/api/search/' + this.query + this.buildSearchQuery())
 			.then(response => {
 				this.searched = true;
 				this.results = response.body.data;
 				this.$router.push({query: {
 					query: this.query,
+					genre: this.searchOptions.genre.length > 0 ?this.searchOptions.genre :undefined,
+					city: this.searchOptions.city.length > 0 && this.locationOption == 'city' ?this.searchOptions.city :undefined,
+					country: this.searchOptions.country.length > 0 && this.locationOption == 'country' ?this.searchOptions.country :undefined,
+					locations: this.searchOptions.includeCategories.locations,
+					events: this.searchOptions.includeCategories.events,
+					bands: this.searchOptions.includeCategories.bands
 				}});
-				
-				// let notEmptyResultsCounter = [];
 
 				for(let category in this.results) {
 					for(let result of this.results[category]) {
@@ -215,6 +230,34 @@ export default {
 			.catch(err => {
 				this.loading = false;
 			});
+		},
+		buildSearchQuery() {
+			let searchQuery = '';
+
+			if(this.searchOptions.city != '' && this.locationOption == 'city')
+				searchQuery += 'city=' + this.searchOptions.city + '&';
+			
+			if(this.searchOptions.country != '' && this.locationOption == 'country')
+				searchQuery += 'country=' + this.searchOptions.country + '&';
+
+			if(this.searchOptions.genre != '')
+				searchQuery += 'genre=' + this.searchOptions.genre + '&';
+
+			let categoriesString = '';
+
+			for(let key in this.searchOptions.includeCategories) {
+				if(this.searchOptions.includeCategories[key])
+					categoriesString += key + ',';
+			}
+
+			categoriesString = categoriesString.length > 0 ?'categories=' + categoriesString.substring(0, categoriesString.length-1) 
+														   :categoriesString;
+
+			searchQuery += categoriesString + '&';
+
+			searchQuery = searchQuery.length > 1 ?'?' + searchQuery.substring(0, searchQuery.length-1) : ''
+
+			return searchQuery;
 		},
 		showResult(result) {
 			this.$store.commit(('setCurrent' + result.category), result.data);
@@ -250,13 +293,48 @@ export default {
 		},
 		changeLocationOption(option) {
 			this.locationOption = option;
+		},
+		toggleAdvancedSearch(show) {
+			this.showAdvancedSearch = show;
+			
+			if(this.showAdvancedSearch) {
+				document.getElementsByClassName('search-options')[0].classList.remove('hide-options');
+				document.getElementById('events_search').style.paddingBottom = '160px';
+				if(!this.searched)
+					document.getElementsByClassName('page-content')[0].classList.add('slide-up');
+			}
+			else {
+				document.getElementsByClassName('search-options')[0].classList.add('hide-options');
+				document.getElementById('events_search').style.paddingBottom = '60px';
+				if(!this.searched)
+					document.getElementsByClassName('page-content')[0].classList.remove('slide-up');
+			}
 		}
 	},
 	mounted() {
+		this.searchOptions = {
+			country: this.$route.query.country ?this.$route.query.country :'',
+			city: this.$route.query.city ?this.$route.query.city :'',
+			genre: this.$route.query.genre ?this.$route.query.genre :'',
+			includeCategories: {
+				events: this.$route.query.events == 'false' ?false :true,
+				locations: this.$route.query.locations == 'false' ?false :true,
+				bands: this.$route.query.bands == 'false' ?false :true,
+			}
+		}
+
+		this.locationOption = this.$route.query.country ?'country' :'city';
+
 		if(this.$route.query.query) {
 			this.query = this.$route.query.query;
 			this.search();
 		}
+
+		this.$http.get(backendUrl + '/api/bands/genres')
+			.then(response => {
+				this.availableGenres = response.body.data;
+			})
+			.catch(err => {})
 
 		this.changeLocationOption(this.locationOption);
 		this.placesAutocomplete1 = places({container: this.$refs.city_input, type: 'city'});
