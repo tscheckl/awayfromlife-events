4cde1866de190f6da99e899291097e0e80930828
Philipp Jäckel <philipp.jaeckel96@googlmail.com>
2018-08-21 19:36:08 +0200

    fixed #37 and also fixed footer (hopefully once and for all)

--- src/components/App-Footer.vue
+++ src/components/App-Footer.vue
@@ -4,6 +4,7 @@
 		<a href="https://www.awayfromlife.com/info/impressum/">Imprint</a>
 		<a href="https://www.awayfromlife.com/info/datenschutz/">Privacy</a>
 		<a href="https://www.awayfromlife.com/info/kontakt/">Contact</a>
+		<div class="icon-info">Sidebar icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 	</div>
 </template>
 
