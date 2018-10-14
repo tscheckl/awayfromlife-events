f20dd8efba5bacbc5c1a7b7cd1b7cbc30564377a
tscheckl <philipp.jaeckel96@googlemail.com>
2018-10-14 23:24:42 +0200

    export getter functions into seperate file

--- src/helpers/backend-getters.js
+++ src/helpers/backend-getters.js
@@ -1,1 +1,30 @@
 //TODO: Getter functions from the backend that only return data. e.g. get location/band options for v-select components.
+import {frontEndSecret, backendUrl} from '@/secrets.js';
+
+function getBandOptions(object) {	
+	fetch(backendUrl + "/api/bands")
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+			
+			for(let band of data.data) {
+				band.label = band.name + ' - ' + band.origin.country;
+			}
+			return data.data;
+		});
+}
+
+function getLocationOptions() {
+	fetch(backendUrl + "/api/locations")
+		.then(response => {
+			for(let location of response.body.data) {
+				location.label = location.name + ' - ' + location.address.city;
+			}
+			return response.body.data;
+		})
+		.catch(err => {});
+}
+
+export {getBandOptions, getLocationOptions};
