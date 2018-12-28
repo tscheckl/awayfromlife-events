cfdd9e3c24b5ee54573d00ed84d2b0d8bff49071
Philipp Jäckel <philipp.jaeckel96@googlmail.com>
2018-12-28 20:10:51 +0100

    netlfify V - the dummheit strikes back

--- src/create-env.js
+++ src/create-env.js
@@ -1,2 +1,2 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `API_KEY=${process.env.AUTH_KEY}\n`)
+fs.writeFileSync('./.env', `AUTH_KEY=${process.env.AUTH_KEY}\n BACKEND_URL=${process.env.BACKEND_URL}\n`)
