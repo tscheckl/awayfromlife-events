62540a33e9289c1c7270eccf4e97b42b092349bf
tscheckl <philipp.jaeckel@vizzlo.com>
2019-11-28 22:28:13 +0100

    implement chip component

---
+++ src/components/Utilities/Chip.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="chip" v-on:click="$emit('remove')">
+		<div>
+			<slot></slot>
+			<md-icon>close</md-icon>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'chip'
+}
+</script>
+
+<style lang="scss">
+	@import "src/scss/Utilities/_chip.scss";
+</style>
